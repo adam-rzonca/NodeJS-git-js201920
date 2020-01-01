d7104def69e0051693e250ad3f14d403e2851637
Adam Rzońca <adam.rzonca@gmail.com>
2020-01-01 19:17:39 +0100

    Random

--- ExamTask1/app.js
+++ ExamTask1/app.js
@@ -1,8 +1,10 @@
 const addCommand = require("./add");
+const randomCommand = require("./random");
 
 require("yargs")
   .usage("Usage: $0 <command>")
   .command(addCommand)
+  .command(randomCommand)
   .demandCommand() // Jeśli nie podamy żadnej komendy, wyświetli nam Usage i Help
   .strict() // Nie dopuszczamy parametrów innych, niż wymagane przez komendę
   .help().argv;
